263f9d73b5fc4d1f9f928ebdcc6c95095a9a1382
Phong Nguyen <nhp1807@gmail.com>
2023-06-13 16:47:52 +0700

    Add assignment1306

---
+++ assignment1306/practice/file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <dirent.h>
+
+
+int main(){
+    DIR *dir = opendir(".");
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if(entry->d_type == 8){
+            printf("%s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+    
+}
